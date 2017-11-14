f64d378ab5d55b141aa7dd804d2fe37f4f91aec9
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-11-14 14:56:25 -0800

    create DB - enable Migration

--- VideoShop/Models/Customer.cs
+++ VideoShop/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace VideoShop.Models
     public class Customer
     {
         public int Id { get; set; }
-        public String Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } // foreign key
     }
 }
--- VideoShop/Models/IdentityModels.cs
+++ VideoShop/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace VideoShop.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
