ce1501e91b975ce0073700424091a59dda0a4d19
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-11-14 15:28:36 -0800

    Fixed getting Customer data

--- VideoShop/Controllers/MovieController.cs
+++ VideoShop/Controllers/MovieController.cs
@@ -14,23 +14,23 @@ namespace VideoShop.Controllers
 
         public MovieController()
         {
-            _context = new ApplicationDbContext();
+            //_context = new ApplicationDbContext();
         }
 
         protected override void Dispose(bool disposing)
         {
-            _context.Dispose();
+            //_context.Dispose();
         }
 
         // GET: Movie
         public ActionResult Index()
         {
-            var movies = _context.Movies.ToList();
+            var movies = GetMovies();
             return View(movies);
         }
         public ActionResult Details(int id)
         {
-            var movie =  _context.Movies.FirstOrDefault(m => m.Id == id);
+            var movie =  GetMovies().FirstOrDefault(m => m.Id == id);
             if (movie == null)
             {
                 return HttpNotFound();
@@ -39,25 +39,25 @@ namespace VideoShop.Controllers
             return View(movie);
         }
 
-        /*private IEnumerable<Movie> GetMovies()
+        private IEnumerable<Movie> GetMovies()
         {
             return new List<Movie>
             {
                 new Movie {Id = 100, Name = "Friends"},
                 new Movie {Id = 200, Name = "Fosters"},
             };
-        }*/
+        }
 
         // GET: Movies/Random
         public ActionResult Random()
         {
-            RandomMovieViewModel viewModel = new RandomMovieViewModel()
+            /*RandomMovieViewModel viewModel = new RandomMovieViewModel()
             {
                 Movies = _context.Movies.ToList(),
                 Customers = _context.Customers.ToList()
-            };   
-            
-            return View(viewModel);
+            };   */
+
+            return View();
         }
 
     }
