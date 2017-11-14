5fe3bea38e0602407560bf10c8ef018f6d02131f
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-11-14 11:27:14 -0800

    add model + controller: get movies and customers data and shows in the view

---
+++ VideoShop/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoShop.Models;
+
+namespace VideoShop.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        // a list of customers
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Sarah"},
+                new Customer {Id = 2, Name = "David"}
+            };
+        }
+
+    }
+}
