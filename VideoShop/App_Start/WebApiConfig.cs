97531df0b8ecd5727460411454a73f63f344ac97
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-11-17 14:35:24 -0800

    Chanage format on API - to return json object using camel notation

--- VideoShop/App_Start/WebApiConfig.cs
+++ VideoShop/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace VideoShop
 {
@@ -9,6 +11,11 @@ namespace VideoShop
     {
         public static void Register(HttpConfiguration config)
         {
+            // to chanage format on API to return jsonobject using camel notation
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
